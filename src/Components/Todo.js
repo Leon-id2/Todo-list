e957c6c178939622ffdf3494d0d929cd62b7ddfb
Leon-id2 <kingsleon250@gmail.com>
2024-01-06 12:38:39 +0200

    weather changes

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
  
@@ -8,7 +8,7 @@ export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
     <div className="Todo">
         <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
         <div>
-        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+        
         <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
         </div>
     </div>
